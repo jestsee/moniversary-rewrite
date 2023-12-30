d9dda3698eefeb698caf88f84e8b93d8f04c0ef7
Jesica <jesica@btpn.com>
2023-12-30 17:09:42 +0700

    feat: UI setup

---
+++ components/Header.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+const { scrollTo } = useScroll();
+</script>
+<template>
+  <div
+    class="mx-auto flex h-full flex-col items-center justify-center md:flex-row xl:w-[1240px]"
+  >
+    <img
+      class="-ml-12 -mr-8 hidden md:block md:h-[520px] lg:mr-0 lg:h-[560px]"
+      src="/peep-16.svg"
+      alt="Hbd"
+    />
+    <div class="flex flex-col gap-y-7 lg:w-[600px]">
+      <h1
+        class="-mt-16 text-[13vw] font-extrabold leading-[1.15] md:text-[8vw] lg:mt-0 lg:text-8xl"
+      >
+        Happy Moniversary
+      </h1>
+      <p class="font-semibold">
+        Disclaimer dulu kalo ini tampilannya lebih lengkap kalo buka di laptop.
+        Akhirnya ya mon udah legal beli miras gosah minjem KTP meimei..
+      </p>
+      <button
+        @click="scrollTo('wishes')"
+        class="max-w-fit rounded-full bg-black py-3 px-6 font-semibold text-white underline-offset-4 hover:scale-110 hover:underline"
+      >
+        yuk cek 👇
+      </button>
+    </div>
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,9 @@
 <template>
-  <div class="">aaa</div>
+  <div id="header" class="mx-10 mr-16 flex h-[100vh] bg-white pt-24 lg:block">
+    <Header />
+  </div>
+  <div id="wishes" class="bg-gray-100 px-10 pt-16 lg:pt-20">
+    disini wish view
+  </div>
+  <div id="footer" class="bg-black px-10 text-white"><Footer/></div>
 </template>
