51cedc0062fd2115119d8010a57e9a1f92724a8e
Jesica <jesica@btpn.com>
2023-12-30 16:46:23 +0700

    initial commit

---
+++ composables/useScroll.ts
@@ -0,0 +1,17 @@
+import { ref } from "vue";
+
+const useScroll = () => {
+  const scrollPosition = ref(0);
+
+  const updateScroll = () => {
+    scrollPosition.value = window.scrollY;
+  };
+  const scrollTo = (navEl: string) => {
+    let element = document.querySelector(`#${navEl}`);
+    element?.scrollIntoView({ behavior: "smooth" });
+  };
+
+  return { scrollTo, updateScroll, scrollPosition }
+}
+
+export { useScroll }
