5130fb4b7a4f0eae8917fffc2ce00a0ff8ef80ae
Jesica <jesica@btpn.com>
2024-01-02 14:55:54 +0700

    feat: skeleton loading when load wishes data

---
+++ components/WishesLoading.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+function generateHeight(index: number): string {
+  const numbers = ["h-[160px]", "h-[240px]", "h-[320px]", "h-[400px]"];
+  return numbers[index % 4];
+}
+</script>
+<template>
+  <div class="mx-auto flex w-full gap-x-6 py-10 xl:w-[1240px] h-screen">
+    <div class="sticky w-1/4 top-36 hidden self-start lg:block">
+      <WishesLoadingContainer class="aspect-square" />
+    </div>
+    <div class="columns-1 w-full h-full gap-6 sm:columns-2 xl:columns-3">
+      <template v-for="index in 6">
+        <div class="mb-6 break-inside-avoid">
+          <WishesLoadingContainer :class="generateHeight(index)" />
+        </div>
+      </template>
+    </div>
+  </div>
+</template>
---
+++ components/WishesLoadingContainer.vue
@@ -0,0 +1,3 @@
+<template>
+  <div class="bg-zinc-300 w-full rounded-3xl animate-pulse"></div>
+</template>
